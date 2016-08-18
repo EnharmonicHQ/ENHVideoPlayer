6dc688bbd206e3d46ab71267a2ae0da4fb6367c5
Dillan Laughlin <dlaughlin@enharmonichq.com>
2016-08-18 12:46:43 -0500

    Added umbrella header.

---
+++ VideoRig/VideoRig/ENHAVPlayer/ENHAVPlayer.h
@@ -0,0 +1,21 @@
+//
+//  ENHAVPlayer.h
+//
+//  Created by Dillan Laughlin on 9/4/15.
+//  Copyright © 2015 Enharmonic. All rights reserved.
+//
+
+#ifndef ENHAVPlayer_h
+#define ENHAVPlayer_h
+
+#import "ENHAVAssetLoadingOperation.h"
+#import "ENHAVIdlePlaybackView.h"
+#import "ENHAVPlayerControlsView.h"
+#import "ENHAVPlayerView.h"
+#import "ENHAVPlayerViewController.h"
+#import "ENHPlaybackButton.h"
+#import "ENHTappableSlider.h"
+#import "NSString+ENHTimeInterval.h"
+#import "UIImageView+ENHAVThumbnail.h"
+
+#endif /* ENHAVPlayer_h */
