992bed43b991afe5e417f23aa2094d16f8f79959
Dillan Laughlin <dlaughlin@enharmonichq.com>
2016-09-15 10:03:13 -0500

    Expose full screen active state in header.

--- VideoRig/VideoRig/ENHAVPlayer/ENHAVPlayerViewController.h
+++ VideoRig/VideoRig/ENHAVPlayer/ENHAVPlayerViewController.h
@@ -26,6 +26,7 @@ NS_ASSUME_NONNULL_BEGIN
 @property (nonatomic, weak, readonly) IBOutlet UIView *contentOverlayView;
 @property (readonly, nullable) NSURL *currentPlayerItemURL;
 @property (nonatomic, copy, nullable) void (^singleTapHandler)(UITapGestureRecognizer *tap);
+@property (nonatomic, assign, readonly) BOOL fullScreenActive;
 
 -(void)play;
 -(void)hidePlayerControlsView;
